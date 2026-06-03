2a7e35a8785437418641b77f4eeac9886130ca70
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-06-03 10:02:30 +0000

    add copy.c

---
+++ code/ch04/copy.c
@@ -0,0 +1,58 @@
+#include "tlpi_hdr.h"
+#include <fcntl.h>
+
+/**
+ * We are using ifndef to allow override BUF_SIZE definition
+ * using the cc -D option while compiling
+ */
+#ifndef BUF_SIZE
+#define BUF_SIZE 1024
+#endif
+
+int main(int argc, const char *argv[])
+{
+    // Declare vars
+    int input_fd, output_fd, open_flags;
+    mode_t file_permissions;
+    ssize_t num_read;
+
+    // Buffer
+    char buf[BUF_SIZE];
+
+    // Validate args
+    // Print the usage message if incorrect number of args
+    // or if --help option is given
+    if (argc != 3 || strcmp(argv[1], "--help") == 0)
+        usageErr("%s <old-file> <new-file>\n", argv[0]);
+
+    // Open input file
+    input_fd = open(argv[1], O_RDONLY);
+    if (input_fd == -1)
+        errExit("opening file %s", argv[1]);
+
+    // Open output file with rw-rw-rw- permissions
+    open_flags = O_CREAT | O_WRONLY | O_TRUNC;
+    file_permissions = S_IRUSR | S_IWUSR |
+                       S_IRGRP | S_IWGRP |
+                       S_IROTH | S_IWOTH;
+    output_fd = open(argv[2], open_flags, file_permissions);
+    if (output_fd == -1)
+        errExit("opening file %s", argv[2]);
+
+    // Transfer the data from the old file to the new file
+    // until we reach the end or encounter an error
+    while ((num_read = read(input_fd, buf, BUF_SIZE)) > 0)
+        if (write(output_fd, buf, num_read) != num_read)
+            fatal("couldn't write whole buffer");
+    if (num_read == -1)
+        errExit("read");
+
+    // Close the file descriptors
+    if (close(input_fd) == -1)
+        errExit("close input");
+    if (close(output_fd) == -1)
+        errExit("close output");
+
+    // Exit the program
+    exit(EXIT_SUCCESS);
+}
